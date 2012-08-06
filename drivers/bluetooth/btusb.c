e8cc428d26b31fe46874dcbadbdcb24a72cbaf70
Gustavo Padovan <gustavo.padovan@collabora.co.uk>
2012-08-06 15:36:49 -0300

    Bluetooth: Use USB_VENDOR_AND_INTERFACE() for Broadcom devices
    commit 92c385f46b30f4954e9dd2d2005c12d233b479ea upstream.
    
    Many Broadcom devices has a vendor specific devices class, with this rule
    we match all existent and future controllers with this behavior.
    
    We also remove old rules to that matches product id for Broadcom devices.
    
    Change-Id: Ibec1ebeeabd38f2a545a6fd1d2e7142a96d027e8
    Tested-by: John Hommel <john.hommel@hp.com>
    Signed-off-by: Gustavo Padovan <gustavo.padovan@collabora.co.uk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/bluetooth/btusb.c
+++ drivers/bluetooth/btusb.c
@@ -127,6 +127,9 @@ static struct usb_device_id btusb_table[] = {
 	{ USB_DEVICE(0x8087, 0x0a5a),
 	  .driver_info = BTUSB_INTEL_BOOT | BTUSB_BROKEN_ISOC },
 
+	/*Broadcom devices with vendor specific id */
+	{ USB_VENDOR_AND_INTERFACE_INFO(0x0a5c, 0xff, 0x01, 0x01) },
+
 	{ }	/* Terminating entry */
 };
 
