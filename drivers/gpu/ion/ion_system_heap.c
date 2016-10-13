f76ad612846861bf8ce5345180cb7d46dcadeaf4
Srinivasarao P <spathi@codeaurora.org>
2016-10-13 15:30:33 +0800

    ion: use %pk instead of %p which respects kptr_restrict sysctl
    Hide kernel pointers from unprivileged users by using %pk format-specifier
    instead of %p. This respects the kptr_restrict sysctl setting which is by
    default on. So by default %pk will print zeroes as address. echo 1 to
    kptr_restrict to print proper kernel address.
    
    Change-Id: Ia300e3e38b8662afac15edda28959564b05c9367
    Signed-off-by: Satyajit Desai <sadesai@codeaurora.org>
    Signed-off-by: Srinivasarao P <spathi@codeaurora.org>
    Signed-off-by: Kalpaj Chaudhari <daedroza@gmail.com>

--- drivers/gpu/ion/ion_system_heap.c
+++ drivers/gpu/ion/ion_system_heap.c
@@ -1,9 +1,6 @@
 /*
  * drivers/gpu/ion/ion_system_heap.c
- *
- * Copyright (C) 2011 Google, Inc.
- * Copyright (c) 2011-2015, The Linux Foundation. All rights reserved.
- *
+ * Copyright (c) 2011-2015,2016 The Linux Foundation. All rights reserved.
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
  * may be copied, distributed, and modified under those terms.
@@ -300,7 +297,7 @@ int ion_system_heap_map_iommu(struct ion_buffer *buffer,
 			      buffer->size, prot);
 
 	if (ret) {
-		pr_err("%s: could not map %lx in domain %p\n",
+		pr_err("%s: could not map %lx in domain %pK\n",
 			__func__, data->iova_addr, domain);
 		goto out1;
 	}
@@ -448,7 +445,7 @@ int ion_system_contig_heap_cache_ops(struct ion_heap *heap,
 
 		pstart = virt_to_phys(buffer->priv_virt) + offset;
 		if (!pstart) {
-			WARN(1, "Could not do virt to phys translation on %p\n",
+			WARN(1, "Could not do virt to phys translation on %pK\n",
 				buffer->priv_virt);
 			return -EINVAL;
 		}
@@ -521,7 +518,7 @@ int ion_system_contig_heap_map_iommu(struct ion_buffer *buffer,
 	ret = iommu_map_range(domain, data->iova_addr, sglist,
 			      buffer->size, prot);
 	if (ret) {
-		pr_err("%s: could not map %lx in domain %p\n",
+		pr_err("%s: could not map %lx in domain %pK\n",
 			__func__, data->iova_addr, domain);
 		goto out1;
 	}
